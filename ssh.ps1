f086b1fd135609d36ceba4ad945db843e15dfe4d
Jay <20913004+jlaiii@users.noreply.github.com>
2022-12-11 21:50:48 -0600

    Add files via upload

--- ssh.ps1
+++ ssh.ps1
@@ -1,10 +1,9 @@
-$Title = "SSH"
+$Title = "Easy SSH"
 $host.UI.RawUI.WindowTitle = $Title
 cd c:\windows\system32\openssh
 clear
 $ip= Read-Host "IP"
 $port= Read-Host "Port"
 $user= Read-Host "Username"
-write-host "connecting..."
 clear
 .\ssh -p $port $user@$ip
