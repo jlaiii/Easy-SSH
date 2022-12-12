a28b0997e0e6950f0109ee04dfb59c94bd1e3d9c
Jay <20913004+jlaiii@users.noreply.github.com>
2022-12-12 00:01:52 -0600

    Add files via upload

--- Easy SSH.ps1
+++ Easy SSH.ps1
@@ -1,8 +1,7 @@
 $Title = "Easy SSH"
 $host.UI.RawUI.WindowTitle = $Title
 cd c:\windows\system32\openssh
-for ($num = 1 ; $num -le 99 ; $num++)
-{
+for ($num = 1 ; $num -le 99 ; $num++){
 clear
 write-host " "
 $ip= Read-Host "IP"
@@ -12,6 +11,4 @@ clear
 write-host " "
 .\ssh $user@$ip -p $port
 write-host " "
-pause
-	
-	}
+pause}
