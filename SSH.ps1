d15998bc4e35585bd06ce326fffb6871c901d48a
Jay <20913004+jlaiii@users.noreply.github.com>
2022-12-13 01:50:38 -0600

    Add files via upload

---
+++ SSH.ps1
@@ -0,0 +1,8 @@
+# This script will SSH into a server.
+
+$username = Read-Host "Enter username"
+$server = Read-Host "Enter server address"
+$port = Read-Host "Enter port number"
+
+Write-Host "Connecting to $server on port $port..."
+ssh -l $username $server -p $port
