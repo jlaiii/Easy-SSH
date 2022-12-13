f976d5b23fcf5de38253d5b7c5c4b69db1767d1d
Jay <20913004+jlaiii@users.noreply.github.com>
2022-12-12 23:29:25 -0600

    Add files via upload

--- Easy SSH.ps1
+++ Easy SSH.ps1
@@ -5,10 +5,22 @@ for ($num = 1 ; $num -le 99 ; $num++){
 clear
 write-host " "
 $ip= Read-Host "IP"
+clear
+write-host " "
+$Title = "Easy SSH: connecting to $ip"
+$host.UI.RawUI.WindowTitle = $Title
 $port= Read-Host "Port"
+clear
+$Title = "Easy SSH: connecting to $ip on port $port"
+$host.UI.RawUI.WindowTitle = $Title
+write-host " "
 $user= Read-Host "Username"
 clear
+$Title = "Easy SSH: connecting to $ip on port $port with user $user"
+$host.UI.RawUI.WindowTitle = $Title
 write-host " "
 .\ssh $user@$ip -p $port
 write-host " "
+$Title = "Easy SSH"
+$host.UI.RawUI.WindowTitle = $Title
 pause}
